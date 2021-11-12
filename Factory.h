c67146c9dff39b26350e61841cea935bcb24252f
lMercyl <60775519+lMercyl@users.noreply.github.com>
2021-11-13 02:35:54 +0300

    Update Factory.h

--- Factory.h
+++ Factory.h
@@ -9,8 +9,10 @@ class Factory {
 public:
     Factory();
     virtual ~Factory();
+    
     bool numbersInStr(std::string str);
     bool charInNumbers(std::string str);
+    
     virtual void setInfoObject() = 0;
     virtual void editInfoObject(int id) = 0;
     virtual void showInfoObject() = 0;
